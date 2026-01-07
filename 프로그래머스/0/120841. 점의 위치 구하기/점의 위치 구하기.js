4e90464aada263044e285a75338338a6249742fd
이태연 <greenjuni7@naver.com>
2026-01-07 19:27:46 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,10 @@
+function solution(dot) {
+    let x = dot[0] > 0 ? 1 : 0
+    let y = dot[1] > 0 ? 1 : 0
+    
+    if(x==1 && y==1) return 1
+    if(x==0 && y==1) return 2
+    if(x==0 && y==0) return 3
+    if(x==1 && y==0) return 4
+
+}
