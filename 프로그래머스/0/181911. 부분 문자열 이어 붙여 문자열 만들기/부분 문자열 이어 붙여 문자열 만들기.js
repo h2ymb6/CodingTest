9552b0998e037cd4b027e4946afce223b30da4cf
이태연 <binnimhi@naver.com>
2026-07-17 21:54:59 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.15 ms, Memory: 44.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,8 @@
+function solution(my_strings, parts) {
+    var answer = [];
+    for(let i in my_strings){
+       answer.push(my_strings[i].slice(parts[i][0], parts[i][1]+1))
+    }
+    
+    return answer.join('');
+}
